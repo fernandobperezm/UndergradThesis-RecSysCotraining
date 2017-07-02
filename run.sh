6ffe20093ebb89e4760933643a752c7b1dc50b0c
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-07-02 13:39:41 +0200

    Added a single bash script that executes all the other scripts.

---
+++ run.sh
@@ -0,0 +1,8 @@
+# Installation of Cython.
+cd Configuration/ ; sh install.sh ; cd ..
+
+# Running each recommender in sequence, it may take more time but won't make
+# going out of space while using MovieLens10M, MovieLens20M or Netflix100M.
+cd run-examples/ ; sh holdout-cotraining-knn-knn.sh ; cd ..
+cd run-examples/ ; sh holdout-cotraining-knn-funksvd.sh ;cd ..
+cd run-examples/ ; sh holdout-cotraining-mf-mf.sh ; cd ..
