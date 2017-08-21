8f8c7fd023b9478cc1e245a931814ec4d704357d
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-08-21 16:53:19 +0200

    Removed the binarization as it's buggy.

--- run-examples/holdout-cotraining-knn-slim.sh
+++ run-examples/holdout-cotraining-knn-slim.sh
@@ -86,7 +86,7 @@ python3 ../scripts/holdout.py \
     --number_negatives $NNEGATIVES \
     --number_unlabeled $UNLABELED \
     --params_1 similarity=adj-cosine,k=350,shrinkage=0,normalize=True,sparse_weights=True \
-    --params_2 l2_penalty=0.1,l1_penalty=0.001 \
-    --is_binary --make_binary --binary_th 4.0 #\ -> If the dataset is binary.
+    --params_2 l2_penalty=0.1,l1_penalty=0.001 #\
+    #--is_binary --make_binary --binary_th 4.0 #\ -> If the dataset is binary.
     # --recover_cotraining --recover_iter 10
     #--columns -> Comma separated names for every column.
